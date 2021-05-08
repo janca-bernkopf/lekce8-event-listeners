e3e02699e728fb428f84447f243412e0c9d669f7
Jana Bernkopf <jana.bieles.bernkopf@gmail.com>
2021-05-08 15:16:19 +0200

    data atributy

---
+++ ovoce.js
@@ -0,0 +1,4 @@
+let prvniOvoce = document.querySelector("#muj-div").dataset.ovoce;
+let prvniOvoceJinak = document.querySelector("#muj-div").getAttribute("data-ovoce");
+console.log(prvniOvoce);
+console.log(prvniOvoceJinak);
